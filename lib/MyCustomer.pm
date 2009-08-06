734b4e98e48f1d0272a1b6289be86e0fbccf802e
Lester Ariel Mesa <lesterm@gmail.com>
2009-08-06 17:39:16 -0400

    Adding MyCustomer package

---
+++ lib/MyCustomer.pm
@@ -0,0 +1,26 @@
+package MyCustomer;
+
+use Moose;
+use MooseX::Types::Moose qw(Str Int ArrayRef);
+use namespace::autoclean;
+use Data::Dumper;
+
+has 'user'      => ( is => 'ro', required => 1, weak_ref => 1 );
+has 'session'   => ( is => 'ro', required => 1, weak_ref => 1 );
+has 'schema'    => ( is => 'rw', required => 1, handles => [qw / resultset /] );
+has 'sku'       => ( is => 'ro', isa => Str );
+has 'qty'       => ( is => 'ro', isa => Int, default => 0 );
+
+# create customer
+ 
+# list customer
+
+# get customer
+
+# update customer
+
+# delete customer
+
+
+
+1;
