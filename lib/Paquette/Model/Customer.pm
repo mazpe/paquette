a0a4400d77dad632900239fa16bf81fa659d27d2
Lester Ariel Mesa <lesterm@gmail.com>
2009-08-06 20:28:46 -0400

    Adding Customer model

---
+++ lib/Paquette/Model/Customer.pm
@@ -0,0 +1,43 @@
+package Paquette::Model::Customer;
+
+use Moose;
+use MyCustomer;
+use namespace::autoclean;
+extends 'Catalyst::Model';
+
+with 'Catalyst::Component::InstancePerContext';
+
+=head1 NAME
+
+Paquette::Model::Customer - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head 1 METHODS
+
+=cut
+
+sub build_per_context_instance {
+    my ( $self, $c ) = @_;
+    
+    return MyCustomer->new(
+        user    => $c->user,
+        session => $c->session,
+        schema  => $c->model('PaquetteDB')->schema
+    );
+}
+
+=head1 AUTHOR
+
+Lester Ariel Mesa,,305-402-6717,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
